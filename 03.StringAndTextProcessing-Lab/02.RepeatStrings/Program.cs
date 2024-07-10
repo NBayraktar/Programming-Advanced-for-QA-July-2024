a6751ac2ca7dfb720d75b01050d9da5b52ab5773
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-10 22:47:14 +0100

    Created program wich reads an array of strings from the console. Each string is repeated N times, where N is the length of the string. On the end program prints the concatenated string to the console. Program named: 02.RepeatStrings.

---
+++ 03.StringAndTextProcessing-Lab/02.RepeatStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read array of strings from the console
+            string[] texts = Console.ReadLine().Split(" ").ToArray();
+
+            string result = "";
+            foreach (string text in texts) { 
+                int length = text.Length;
+                for (int i = 0; i < length; i++) { 
+                    result += text; 
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
