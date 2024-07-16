aa7b1589d9031c14b4e15ec54456f4b690a75046
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-16 23:39:29 +0100

    Created program with given name: 03.WordSynonyms. Program keeps a dictionary with synonyms. The key of the dictionary will be the word. The value will be a list of all the synonyms of that word. You will be given a number n - the count of the words. After each word, you will be given a synonym, so the count of lines yhou have to read from the console is 2 * n.

---
+++ 06.DictionariesLambdaAndLINQ/03.WordSynonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read n number from the console (the count of the words)
+            int wordsCount = int.Parse(Console.ReadLine());
+
+            // Creating empty Dictionary
+            Dictionary<string, List<string>> wordSynonyms = new Dictionary<string, List<string>>();
+
+            for (int i = 1; i <= wordsCount; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!wordSynonyms.ContainsKey(word))
+                {
+                    wordSynonyms.Add(word, new List<string>());
+                    wordSynonyms[word].Add(synonym);
+                }
+                else
+                {
+                    wordSynonyms[word].Add(synonym);
+                }
+            }
+
+            foreach (KeyValuePair<string, List<string>> entry in wordSynonyms) {
+                Console.WriteLine($"{entry.Key} - {string.Join(", ", entry.Value)}");
+            }
+        }
+    }
+}
