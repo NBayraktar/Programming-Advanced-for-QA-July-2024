fd837529d3cc4b0f6c5e090ec1d5efa67b7770e2
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-17 18:44:31 +0100

    Created Project: 07.ExerDictionariesLambdaAndLINQ. In the project created program with name: 01.CountCharsInAString. Program counts all characters in a string, except for space (" ").

---
+++ 06.DictionariesLambdaAndLINQ/07.Exer-DictionariesLambdaAndLINQ/01.CountCharsInAString/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read string from the console
+            string text = Console.ReadLine();
+
+            // Count all characters in a string, expect for space (' '), print the occurrences in the following format: {char} -> {occurrences}
+            Dictionary<char, int> countChar = new Dictionary<char, int>();
+            foreach (char symbol in text)
+            {
+                if (symbol == ' ')
+                {
+                    continue;
+                }
+
+                if (!countChar.ContainsKey(symbol))
+                {
+                    countChar.Add(symbol, 1);
+                }
+                else
+                {
+                    countChar[symbol]++;
+                }
+            }
+            foreach (KeyValuePair<char, int> pair in countChar)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
