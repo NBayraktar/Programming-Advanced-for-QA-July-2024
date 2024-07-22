d3af5af77c1c8ce2b5db3189fb3dca1d175f188f
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-22 22:53:11 +0100

    Created program with given name: 06.CompanyUsers. Program receivs a company name and employe id until receives "End" command.

--- 07.ExerDictLambdaLINQ/04.SoftUniParking/obj/Debug/net6.0/04.SoftUniParking.AssemblyInfo.cs
+++ 07.ExerDictLambdaLINQ/04.SoftUniParking/obj/Debug/net6.0/04.SoftUniParking.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04.SoftUniParking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d4b9c38ccf2852cf4b99f04aa5b2cfa6cbf4e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92d68d8f4001034822f638eeeefa2c0e3067a853")]
 [assembly: System.Reflection.AssemblyProductAttribute("04.SoftUniParking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04.SoftUniParking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 07.ExerDictLambdaLINQ/06.CompanyUsers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _06.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read string from the console
+            string input = Console.ReadLine();
+
+            // Dictionary for company users
+            Dictionary<string, List<string>> companyUsers = new Dictionary<string, List<string>>();
+
+            while (input != "End")
+            {
+                string companyName = input.Split(" -> ")[0];
+                string employeeId = input.Split(" -> ")[1];
+
+                if (!companyUsers.ContainsKey(companyName))
+                {
+                    companyUsers.Add(companyName, new List<string>());
+                }
+
+                List<string> employeesList = companyUsers[companyName];
+
+                if (!employeesList.Contains(employeeId))
+                {
+                    employeesList.Add(employeeId);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (KeyValuePair<string, List<string>> entry in companyUsers)
+            {
+                Console.WriteLine($"{entry.Key}");
+                entry.Value.ForEach(id => Console.WriteLine($"-- {id}"));
+            }
+        }
+    }
+}
